4a963e470a31c1ac34b4047adc70980c27ba48c7
dygduck <duyguebcim@yahoo.com>
2018-07-25 17:04:00 -0700

    5 cat images are added and they are clickable.

--- js/cat.js
+++ js/cat.js
@@ -1,29 +1,26 @@
-// Creating a text node to be added to html
-var text1 = document.createTextNode("Cat1");
-// appending the text node to the assigned header
-document.getElementById("header1").appendChild(text1);
-var cat1 = document.getElementById("image1");
-var count1 = 0;
+// Instantly invoked function which increases the click number once the cat image clicked
 
-// click counter function for the cat1
-function counter1() {
-  count1 += 1;
-  document.getElementById("clicks1").innerHTML = count1;
-}
-// adding an event listener to cat1 so once it is clicked the counter1 function will be called and the click number will increase
-cat1.addEventListener("click", counter1);
+var addUp = function(clickId) {
+  var count = 0;
 
+  return function() {
+    var counterElement = document.getElementById(clickId);
+    if (counterElement)
+        counterElement.innerHTML = "Clicks: " + ++count;
+  }
+};
 
-// Creating a text node to be added to html
-var text2 = document.createTextNode("Cat2");
-// appending the text node to the assigned header
-document.getElementById("header2").appendChild(text1);
-var cat2 = document.getElementById("image2");
-var count2 = 0;
-// click counter function for the cat1
-function counter2() {
-  count2 += 1;
-  document.getElementById("clicks2").innerHTML = count2;
-}
-// adding an event listener to cat1 so once it is clicked the counter2 function will be called and the click number will increase
-cat2.addEventListener("click", counter2);
+var catImage1 = document.getElementById("image1");
+catImage1.addEventListener("click", addUp("cat1clicks"));
+
+var catImage2 = document.getElementById("image2");
+catImage2.addEventListener("click", addUp("cat2clicks"));
+
+var catImage3 = document.getElementById("image3");
+catImage3.addEventListener("click", addUp("cat3clicks"));
+
+var catImage4 = document.getElementById("image4");
+catImage4.addEventListener("click", addUp("cat4clicks"));
+
+var catImage5 = document.getElementById("image5");
+catImage5.addEventListener("click", addUp("cat5clicks"));
